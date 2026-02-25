d2968cfa0350dd587c740eb26df64c82415e3fc8
Lucas <142857337+LucasCzx@users.noreply.github.com>
2026-02-25 14:40:08 -0300

    Add files via upload

---
+++ Introduction Python/exerc.py
@@ -0,0 +1,6 @@
+Var = "70"
+
+var1 = "90"
+
+print(Var)
+print(var1)
---
+++ Introduction Python/intro.py
@@ -0,0 +1,11 @@
+nome = "Ana"
+idade = 17
+altura = 1.65
+matriculado = True
+nome = "Bolsonaro"
+
+print(nome)
+print(idade)
+print(altura)
+print(matriculado)
+print(nome)
