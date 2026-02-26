b5049367c3aa7f819a151249d88fd11dfe2338bd
Lucas <142857337+LucasCzx@users.noreply.github.com>
2026-02-26 14:37:37 -0300

    Add files via upload

--- UC - Algoritmo  ( 26-02-2026 )/bool.py
+++ UC - Algoritmo  ( 26-02-2026 )/bool.py
@@ -1,6 +1,7 @@
 resp = input("Você vai passar de ano? S/N ").strip().lower()
 #resposta = bool(resp)
-resposta = (resp == "s")
+#resposta = (resp == "s")
+resposta = resp in ("sim", "s")
 
 print("Resposta do aluno", resp)
 print("Resultado ", resposta)
---
+++ UC - Algoritmo  ( 26-02-2026 )/nome.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome? ")
+
+idade = int(input("Digite sua idade: "))
+
+altura = float(input("Digite sua altura: "))
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
