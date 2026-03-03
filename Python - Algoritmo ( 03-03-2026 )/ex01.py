cbbf0b243c5b85464c9666622bd5fe742247e77f
Lucas <142857337+LucasCzx@users.noreply.github.com>
2026-03-03 14:37:17 -0300

    Add files via upload

---
+++ Python - Algoritmo ( 03-03-2026 )/ex01.py
@@ -0,0 +1,8 @@
+numero_um = float(input("Digite um número: "))
+numero_dois = float(input("Digite outro número: "))
+
+soma = numero_um + numero_um
+produto = numero_um *  numero_dois
+
+print(f"A soma desses números é: {soma}")
+print(f"O produto é igual: {produto}")
