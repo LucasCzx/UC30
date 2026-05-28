140b05bb3ade3caf6c30b15ed1e8265f4a0fdcc3
Lucas <142857337+LucasCzx@users.noreply.github.com>
2026-05-28 14:35:38 -0300

    Add files via upload

---
+++ UC31 ( 28-05-2026 )/app.py
@@ -0,0 +1,36 @@
+from flask import, Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+
+app.secret_key = '123'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+
+        if not email or not senha:
+            flash('Preencha todos os campos', 'erro')
+            return redirect(url_for('login'))
+        
+        if not email or not senha:
+            flash('Preencha todos os campos', 'erro')
+            return redirect(url_for('login'))
+        
+        if email != 'admin@gmail.com' or != '123':
+            flash('Email ou senha incorretos', 'erro')
+            return redirect(url_for('login'))
+        
+        flash('Login realizado com sucesso', 'sucesso')
+        return redirect(url_for('painel'))
+    
+    return render_template('login.html')
+
+@app.route('/painel')
+def painel():
+    return render_template('painel.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
